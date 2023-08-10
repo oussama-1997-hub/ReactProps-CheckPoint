016ac620550269cb03d706d6d2a925b73c87ecfe
Oussama Ben Ali <benalioussama1997@gmail.com>
2023-08-10 23:52:01 +0100

    Initial commit

---
+++ src/players.js
@@ -0,0 +1,20 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdBkin3oTYsJu7fwiVVrzJr-VUsBfggGrzMQ&usqp=CAU",
+    },
+    {
+        name: "Neymar Jr.",
+        team: "Paris Saint-Germain",
+        nationality: "Brazil",
+        jerseyNumber: 10,
+        age: 29,
+        imageUrl: "https://images2.minutemediacdn.com/image/upload/c_crop,w_3520,h_1980,x_0,y_0/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/90min_en_international_web/01g6wk3hc7qnm3bspcsm.jpg",
+      },
+  ];
+  
+  export default players;
